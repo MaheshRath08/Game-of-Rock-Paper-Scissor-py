02d8286d2bfe024d4c94d760c77a6b9f376e7837
MaheshRath08 <maheshrath.2002@gmail.com>
2022-06-25 21:48:02 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import random
+
+choices = ["rock", "paper", "scissor"]
+
+compGuess = random.choice(choices)
+
+player = None
+tie = False
+
+myGuess = input("Your choice: rock(r), paper(p), scissor(s):\n").lower()
+
+if myGuess == 'r':
+    player = "rock"
+elif myGuess == 'p':
+    player = "paper"
+elif myGuess == 's':
+    player = "scissor"
+
+if player == compGuess:
+    tie = True
+    myGuess = input("Your choice: rock(r), paper(p), scissor(s):\n").lower()
+
+def game(player, compGuess):
+    if tie:
+        print("You Guessed:", player)
+        print("computer guessed:", compGuess)
+        print("It's a Tie!!!!")
+    elif player == "rock":
+        print("You Guessed:", player)
+        print("computer guessed:", compGuess)
+        if compGuess == "scissor":
+            print("You win!!!!")
+        elif compGuess == "paper":
+            print("You lost!!!!")
+    elif player == "paper":
+        print("You Guessed:", player)
+        print("computer guessed:", compGuess)
+        if compGuess == "rock":
+            print("You win!!!!")
+        elif compGuess == "scissor":
+            print("You lost!!!!")
+    elif player == "scissor":
+        print("You Guessed:", player)
+        print("computer guessed:", compGuess)
+        if compGuess == "paper":
+            print("You win!!!!")
+        elif compGuess == "rock":
+            print("You lost!!!!")
+
+if player not in choices:
+    myGuess = input("Your choice: rock(r), paper(p), scissor(s):\n").lower()
+else:
+    game(player, compGuess)
